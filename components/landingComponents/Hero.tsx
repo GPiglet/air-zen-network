e7fdee80b0f41d98653854cb105d1d0b90dd710b
Smarttabdev <87866630+Smarttabdev@users.noreply.github.com>
2022-08-07 19:11:22 -0700

    fix some issues

--- components/landingComponents/Hero.tsx
+++ components/landingComponents/Hero.tsx
@@ -8,50 +8,103 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 const Hero: FC = () => {
-   //Animate With GSAP
-    const circleRef1 = React.useRef<any>();
-    const circleRef2 = React.useRef<any>();
-    const circleRef3 = React.useRef<any>();
-    const circleRef4 = React.useRef<any>();
-    const circleRef5 = React.useRef<any>(); 
+   //Animate Ref
+   const circleRef1 = React.useRef<any>();
+   const circleRef2 = React.useRef<any>();
+   const circleRef3 = React.useRef<any>();
+   const circleRef4 = React.useRef<any>();
+   const circleRef5 = React.useRef<any>();
 
-    useEffect(() => {
-        gsap.to( circleRef1.current, { 
-            scale: 2, 
-            opacity: 0, 
-            transformOrigin: "50% 50%", 
-            duration: 3, y: '200px', ease: 'linear', 
+   useEffect(() => {       
+        gsap.to(circleRef5.current, {
+            duration: 2,
+            scale: 0.2,
+            opacity: 0,
+            transformOrigin: "50% 50%",
             scrollTrigger: {
-              scrub: true,
-              trigger: circleRef1.current,
-              start: 0,
-              end: 700,
-              // toggleActions: 'play none none reverse'
-          } }, 
-          
-        );
-    
+                trigger: circleRef5.current,
+                markers: true,
+                start: 'top 5%',
+                end: 'bottom 5%',
+                toggleActions: 'restart resume reverse reset'
+            },
+            stagger: 1
+        })
+        gsap.to(circleRef3.current, {
+            duration: 2,
+            scale: 0.2,
+            opacity: 0,
+            transformOrigin: "50% 50%",
+            scrollTrigger: {
+                trigger: circleRef3.current,
+                markers: true,
+                start: 'top 5%',
+                end: 'bottom 5%',
+                toggleActions: 'restart resume reverse reset'
+            },
+            stagger: 1
+        })
 
-    }, [])
+    gsap.to(circleRef2.current, {
+        duration: 2,
+        scale: 2,
+        opacity: 0,
+        transformOrigin: "50% 50%",
+        scrollTrigger: {
+            trigger: circleRef2.current,
+            markers: true,
+            start: 'top 5%',
+            end: 'bottom 5%',
+            toggleActions: 'restart resume reverse reset'
+        },
+        stagger: 1
+    })  
+    gsap.to(circleRef1.current, {
+        duration: 2, y: '50px', ease: 'linear', 
+        opacity: 0,
+        transformOrigin: "50% 50%",
+        scrollTrigger: {
+            trigger: circleRef1.current,
+            markers: true,
+            start: 'top 5%',
+            end: 'bottom 5%',
+            toggleActions: 'restart resume reverse reset'
+        },
+        stagger: 1
+    })  
+    gsap.to(circleRef1.current, {
+        duration: 2, y: '250px', ease: 'linear', 
+        opacity: 0,
+        scrollTrigger: {
+            trigger: circleRef5.current,
+            markers: true,
+            start: 'top 25%',
+            end: 'bottom 75%',
+            toggleActions: 'restart resume reverse reset'
+        },
+        stagger: 1
+    })  
+}, [])
+  
 
     return (
             <section className="container mx-auto pb-16 relative  pt-32 ">
                 <svg className="absolute right-[-52%] bottom-[-56%] sm:top-[20%] md:top-[-5%] lg:top-[-16%] md:w-[70%]   sm:w-[120%] sm:right-[-20%] w-[174%] md:right-[-27%] lg:right-[-25%] md:w-[90%] " viewBox="0 0 1245 2114" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path opacity="0.33" d="M50.0812 701.846C55.2566 989.408 335.109 1200.06 627.722 1301.88C920.334 1403.71 1234.95 1659.84 1242.99 2106.91C1245.57 2250.91 1245.77 155.351 1242.99 0.000182753C1013.21 121.659 900.23 166.66 594.437 171.818C288.643 176.976 44.9057 414.284 50.0812 701.846Z" fill="url(#paint0_linear_967_2268)" />
-                    <circle opacity="0.7" ref={circleRef1} cx="511.729" cy="696.732" r="346.322" fill="url(#paint1_radial_967_2268)" />
+                    <path ref={circleRef1} opacity="0.33" d="M50.0812 701.846C55.2566 989.408 335.109 1200.06 627.722 1301.88C920.334 1403.71 1234.95 1659.84 1242.99 2106.91C1245.57 2250.91 1245.77 155.351 1242.99 0.000182753C1013.21 121.659 900.23 166.66 594.437 171.818C288.643 176.976 44.9057 414.284 50.0812 701.846Z" fill="url(#paint0_linear_967_2268)"/>
+                    <circle ref={circleRef2} opacity="0.7" cx="511.729" cy="696.732" r="346.322" fill="url(#paint1_radial_967_2268)" />
                     <foreignObject  className="font-lato-light  italic w-[260px] text-lg text-white hidden md:block" x="68%" y="23.4%" width="260px" height="100px">
                         <p 
                         >Dieses Feature ermöglicht eine besondere Funktion: XYZ</p>
                     </foreignObject>
                     <image href="/images/sparkle.svg" className="w-[49px] h-[47px] hidden md:block"  x="64.5%" y="23%" cx="511.828" cy="696.377" />
-                    <circle opacity="0.3" cx="512.408" cy="696.273" r="362.358" stroke="#7BB690" />
+                    <circle ref={circleRef3} opacity="0.3" cx="512.408" cy="696.273" r="362.358" stroke="#7BB690" />
                     <image href="/images/sparkle.svg" className="w-[50px] h-[47px] hidden md:block"  x="59.5%" y="43%" cx="511.828" cy="696.377" />
                     <foreignObject  className="font-lato-light  italic w-[260px] text-lg text-white hidden md:block" x="63%" y="43.4%" width="260px" height="100px">
                         <p 
                         >Dieses Feature ermöglicht eine besondere Funktion: XYZ</p>
                     </foreignObject>
-                    <path opacity="0.6" d="M1023.68 696.224C1023.68 978.288 794.632 1206.95 512.089 1206.95C229.545 1206.95 0.5 978.288 0.5 696.224C0.5 414.16 229.545 185.5 512.089 185.5C794.632 185.5 1023.68 414.16 1023.68 696.224Z" stroke="url(#paint2_radial_967_2268)" />
-                    <circle opacity="0.7" cx="511.828" cy="696.377" r="259.285" transform="rotate(-90.105 511.828 696.377)" fill="url(#paint3_radial_967_2268)" />
+                    <path ref={circleRef4} opacity="0.6" d="M1023.68 696.224C1023.68 978.288 794.632 1206.95 512.089 1206.95C229.545 1206.95 0.5 978.288 0.5 696.224C0.5 414.16 229.545 185.5 512.089 185.5C794.632 185.5 1023.68 414.16 1023.68 696.224Z" stroke="url(#paint2_radial_967_2268)" />
+                    <circle ref={circleRef5} opacity="0.7" cx="511.828" cy="696.377" r="259.285" transform="rotate(-90.105 511.828 696.377)" fill="url(#paint3_radial_967_2268)" />
                     <image href="/images/model.png"  x="28.5%" y="25%" cx="511.828" cy="696.377"  transform="translate(0,0)"/>
                     <defs>
                         <linearGradient id="paint0_linear_967_2268" x1="278.213" y1="680.928" x2="1245.06" y2="732.717" gradientUnits="userSpaceOnUse">
