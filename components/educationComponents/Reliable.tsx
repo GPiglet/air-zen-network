9790e91d443f52b65ec1d0c06a7bbc2f88e29aa8
gBrain2026 <107259158+gBrain2026@users.noreply.github.com>
2022-08-09 04:53:01 -0700

    education viewBox reset for animation

--- components/educationComponents/Reliable.tsx
+++ components/educationComponents/Reliable.tsx
@@ -1,19 +1,46 @@
 //modules
 import React, { FC } from "react"
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Reliable: FC = () => {
 
+
+    // const tl = gsap.timeline({
+    //     scrollTrigger: {
+    //       trigger: ".trigger",
+    //       start: "center bottom",
+    //       end: "center top",
+    //       scrub: 3,
+    //       markers: true
+    //     }
+    // });
+    
+    // React.useEffect(() => {
+        
+    //     tl.to(".box", {
+    //             duration: 2,
+    //             scale: 1.5,
+    //             opacity: 0,
+    //             transformOrigin: "50% 50%",
+    //             stagger: 1
+    //         })
+    
+    // },[])
+
     return (
-        <div className="container mx-auto relative mb-[80%] sm:mb-[60%] md:mb-[0%] xl:mb-[10%]">
+        <div className="container mx-auto relative mb-[80%] sm:mb-[100%] md:mb-[0%] xl:mb-[10%]">
             
-            <svg className="absolute
-            left-[5%] sm:left-[15%] md:left-[-25%] lg:left-[-30%] xl:left-[-25%]
-            top-[100%] md:top-[5%] lg:top-[-9%] xl:top-[-50%]
-            w-[80%] sm:w-[60%] md:w-[65%] lg:w-[70%] xl:w-[60%]" viewBox="0 0 723 797" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path opacity="0.4" d="M497.478 697.949C666.249 643.996 760.109 465.886 707.12 300.129C654.13 134.372 474.358 43.7363 305.587 97.689C136.816 151.642 42.9562 329.752 95.9454 495.509C148.935 661.266 328.707 751.902 497.478 697.949Z" fill="url(#paint0_radial_1376_7045)"/>
+            <svg className="absolute trigger 
+            left-[-50%] xs:left-[-50%] sm:left-[-60%] md:left-[-75%] lg:left-[-90%] xl:left-[-71%]
+            top-[93%] xs:top-[80%] sm:top-[140px] md:top-[-25%] lg:top-[-55%] xl:top-[-125%]
+            w-[200%] xs:w-[200%] sm:w-[220%] md:w-[170%] lg:w-[190%] xl:w-[160%]" viewBox="-600 -350 2000 1500" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path className="box" opacity="0.4" d="M497.478 697.949C666.249 643.996 760.109 465.886 707.12 300.129C654.13 134.372 474.358 43.7363 305.587 97.689C136.816 151.642 42.9562 329.752 95.9454 495.509C148.935 661.266 328.707 751.902 497.478 697.949Z" fill="url(#paint0_radial_1376_7045)"/>
                 <path opacity="0.5" d="M474.04 622.595C600.747 582.09 671.214 448.371 631.432 323.926C591.649 199.481 456.682 131.435 329.975 171.941C203.267 212.447 132.801 346.166 172.583 470.61C212.366 595.055 347.332 663.101 474.04 622.595Z" fill="url(#paint1_radial_1376_7045)"/>
                 <path d="M586.13 339.163C618.068 439.068 561.499 546.433 459.757 578.958C358.015 611.483 249.656 556.841 217.719 456.937C185.781 357.032 242.35 249.667 344.092 217.142C445.834 184.617 554.193 239.259 586.13 339.163Z" stroke="url(#paint2_linear_1376_7045)"/>
-                <image href="/images/phone-home.png" x="33%" y="18%" fillOpacity='0.5' className="w-[48%]" />
+                <image href="/images/phone-home.png" x="11%" y="10%" fillOpacity='0.5' className="w-[18%]" />
                 <defs>
                 <radialGradient id="paint0_radial_1376_7045" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(332.077 121.279) rotate(72.664) scale(635.151 646.7)">
                 <stop stopColor="#2294C3"/>
