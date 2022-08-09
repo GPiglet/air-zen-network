f81dce8337e4981e5ed8fb7dccc6b645dde2dfbd
gKoala2026 <107259158+gKoala2026@users.noreply.github.com>
2022-08-09 04:57:25 -0700

    reset viewBox  in education (#8)
    * industrial page mobile responsive complete version
    
    * education mobile responsive complete
    
    * remove minWidth and freeSpace in education/contact
    
    * rename industrial component
    
    * education viewBox reset for animation
    
    Co-authored-by: gBrain2026 <107259158+gBrain2026@users.noreply.github.com>
    Co-authored-by: zenithorr <87866630+zenithorr@users.noreply.github.com>

--- components/educationComponents/Contact.tsx
+++ components/educationComponents/Contact.tsx
@@ -9,15 +9,15 @@ const Contact: FC = () => {
         <div className="relative mt-[100%] sm:mt-[45%] md:mt-[60%] lg:mt-[50%]">
             <div className="container mx-auto relative">
                 <svg className="absolute
-                top-[-100%] xs:top-[-90%] sm:top-[-116%] md:top-[-77%] lg:top-[-82%] xl:top-[-163%]
-                xs:w-[100%] sm:w-[70%] md:w-[70%] lg:w-[60%] max-w-[962px]
-                left-[0] sm:left-[13%] md:left-[-5%] lg:left-0" 
-                viewBox="0 0 909 901" fill="none" xmlns="http://www.w3.org/2000/svg">
+                top-[-78%] xs:top-[-127%] sm:top-[-565px] md:top-[-130%] lg:top-[-150%] xl:top-[-250%]
+                w-[220%] sm:w-[170%] md:w-[160%] lg:w-[160%] xl:w-[150%]
+                left-[-60%] sm:left-[-38%] md:left-[-55%] lg:left-[-55%] xl:left-[-53%]" 
+                viewBox="-600 -350 2000 1500" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path opacity="0.7" d="M376.09 41.1328C598.38 -1.14335 813.492 148.063 856.541 374.416C899.59 600.769 754.268 818.524 531.979 860.8C309.689 903.076 94.5768 753.871 51.5279 527.517C8.47893 301.164 153.8 83.4089 376.09 41.1328Z" stroke="url(#paint0_linear_0_1)"/>
                     <path opacity="0.3" d="M562.868 790.362C753.811 729.321 860.001 527.812 800.051 340.279C740.1 152.746 536.711 50.2033 345.768 111.244C154.825 172.284 48.6346 373.793 108.585 561.326C168.536 748.86 371.925 851.402 562.868 790.362Z" fill="url(#paint1_radial_0_1)"/>
                     <path opacity="0.5" d="M526.114 683.802C652.351 643.446 722.556 510.224 682.921 386.241C643.287 262.259 508.821 194.465 382.584 234.821C256.347 275.176 186.142 408.398 225.777 532.381C265.411 656.364 399.877 724.157 526.114 683.802Z" fill="url(#paint2_radial_0_1)"/>
                     <path opacity="0.5" d="M639.605 400.089C671.721 500.553 614.838 608.535 512.505 641.249C410.172 673.963 301.199 619.002 269.082 518.537C236.966 418.073 293.849 310.091 396.182 277.377C498.516 244.663 607.489 299.624 639.605 400.089Z" stroke="url(#paint3_linear_0_1)" strokeWidth="2"/>
-                    <image href="/images/model-group.png" x="22.5%" y="35%" fillOpacity='0.5' className="w-[55%]" />
+                    <image href="/images/model-group.png" x="10.6%" y="20%" fillOpacity='0.5' className="w-[24%]" />
                     <defs>
                     <linearGradient id="paint0_linear_0_1" x1="133.393" y1="277.702" x2="689.269" y2="774.1" gradientUnits="userSpaceOnUse">
                     <stop stopColor="#159BDE"/>
