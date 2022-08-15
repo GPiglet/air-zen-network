0752f32e0490d951209ac7f8e9e5657771504b99
gBrain2026 <107259158+gBrain2026@users.noreply.github.com>
2022-08-14 23:21:26 -0700

    animation-edu page complete

--- components/common/footer/index.tsx
+++ components/common/footer/index.tsx
@@ -36,7 +36,7 @@ const Footer: FC = () => {
     ]
 
     return (
-            <div className='container m-auto md:fixed md:hidden footer'>
+            <div className='container m-auto footer'>
                 <div className='relative py-[100px] md:py-[100px] flex justify-center overflow-x-hidden'>
             <h1 className="text-title-sm">Impressum</h1>
             </div>
