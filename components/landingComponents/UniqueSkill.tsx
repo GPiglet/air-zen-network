5d2a47079d4d145fb48976c83e3dd4ff4b76b537
Piglet <piglet20181112@outlook.com>
2022-08-21 08:34:14 -0700

    fix width of desc and ui

--- components/landingComponents/UniqueSkill.tsx
+++ components/landingComponents/UniqueSkill.tsx
@@ -187,8 +187,8 @@ const UniqueSkill: FC = () => {
                                                 </picture>
                                                 <p className='font-lato text-[22px] uppercase unique-skill-title pb-3'>{item.title}</p>
                                                 <p className='font-lato-light font-bold text-xl w-[60%] md:w-[90%] pb-3'>{item.subtitle}</p>
-                                                <p className='hidden font-lato font-light text-xl text-lg unique-skill-description pb-3'>{item.description}</p>
-                                                <ul className='hidden list-disc pl-2 unique-skill-list '>
+                                                <p className='hidden font-lato font-light text-xl text-lg unique-skill-description pb-3 md:w-[280px]'>{item.description}</p>
+                                                <ul className='hidden list-disc pl-2 unique-skill-list md:w-[280px]'>
                                                     {item.list.split('\n').map((subitem: string, ind: number) => (
                                                         <li className='font-lato text-[16px] font-extralight leading-6' key={ind}>{subitem}</li>
                                                     ))}
