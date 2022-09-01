ef59991c8906888350edab8d53fcda20e7951a97
zenithorr <87866630+zenithorr@users.noreply.github.com>
2022-09-01 08:25:58 -0700

    solutions css animation desktop remove (#38)
    * multi language
    
    * home subpage form ongoing
    
    * home subpage form section demo
    
    * multilang
    
    * overflowclip
    
    * deploy error fixed
    
    * deploy error fixed
    
    * deploy error fixed
    
    * deploy error fixed
    
    * industrial subpage backgroundcolor
    
    * fix aboutus touch issue
    
    * multi language demo
    
    * multi language demo
    
    * multi language demo
    
    * multi language demo
    
    * fix some issue for deploy
    
    * fix some issue for deploy
    
    * fix some issue for deploy
    
    * multilanguage key modified
    
    * subpage Navitem & equal box sizing & footer missing & back to solution anchor
    
    * navbar blur
    
    * navbar blur
    
    * font-lato-italic& solution box opening & ..
    
    * mobile top menu hide show &&
    
    * checkbox custom
    
    * checkbox custom
    
    * top navbar global
    
    * story tail fix
    
    * landing page layout fixed
    
    * landing page layout fixed
    
    * about us layout fixed
    
    * solutions desktop ani css remove
    
    Co-authored-by: Smarttabdev <87866630+Smarttabdev@users.noreply.github.com>

--- components/homeComponents/SafeHome.tsx
+++ components/homeComponents/SafeHome.tsx
@@ -6,10 +6,12 @@ import { useTranslation } from "next-i18next";
 const SafeHome: FC = () => {
     //translate
     const { t } = useTranslation()
+    const easyList = t('home.easy.list', { returnObjects: true })
+
 
     return (
-        <section id="secure" className="container mx-auto relative">
-            <svg className="absolute left-[-27%] bottom-[-28%] w-[140%] sm:top-[37%]  md:top-1/2 md:translate-y-[-50%] sm:w-[120%] md:w-[100%] xl:w-[65%]   sm:left-[-10%]  md:left-[-45%] xl:left-[-20%]" viewBox="0 0 804 796" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div id="secure" className="container mx-auto relative md:h-screen">
+            <svg className="hidden md:block absolute left-[-27%] bottom-[-28%] w-[140%] sm:top-[37%]  md:top-1/2 md:translate-y-[-50%] sm:w-[120%] md:w-[100%] xl:w-[90%]   sm:left-[-10%]  md:left-[-45%] xl:left-[-40%]" viewBox="0 0 804 796" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path opacity="0.4" d="M497.478 697.95C666.249 643.997 760.109 465.887 707.12 300.13C654.13 134.373 474.358 43.7373 305.587 97.69C136.816 151.643 42.9562 329.753 95.9454 495.51C148.935 661.267 328.707 751.903 497.478 697.95Z" fill="url(#paint0_radial_1376_4769)" />
                 <path opacity="0.5" d="M474.04 622.595C600.747 582.09 671.214 448.371 631.432 323.926C591.649 199.481 456.682 131.435 329.975 171.941C203.267 212.447 132.801 346.166 172.583 470.61C212.366 595.055 347.332 663.101 474.04 622.595Z" fill="url(#paint1_radial_1376_4769)" />
                 <path d="M586.13 339.163C618.068 439.068 561.499 546.433 459.757 578.958C358.015 611.483 249.656 556.841 217.719 456.937C185.781 357.032 242.35 249.667 344.092 217.142C445.834 184.617 554.193 239.258 586.13 339.163Z" stroke="url(#paint2_linear_1376_4769)" />
@@ -30,6 +32,36 @@ const SafeHome: FC = () => {
                 </defs>
             </svg>
 
+            
+            <svg className="absolute right-[-52%] top-[57%] sm:top-[32%] md:top-[26%] md:w-[120%] xl:w-[100%]   sm:w-[140%] sm:right-[-20%] w-[204%] md:right-[-70%] xl:right-[-53%] " viewBox="0 0 907 907" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path opacity="0.4" d="M538.472 704.59C686.17 656.921 768.31 499.556 721.937 353.106C675.564 206.655 518.239 126.576 370.541 174.245C222.842 221.913 140.702 379.278 187.075 525.729C233.448 672.179 390.774 752.258 538.472 704.59Z" fill="url(#paint0_radial_1376_4815)" />
+                <path opacity="0.5" d="M512.183 639.605C615.95 606.114 673.659 495.555 641.079 392.664C608.499 289.773 497.968 233.513 394.2 267.003C290.433 300.493 232.724 411.052 265.304 513.943C297.884 616.834 408.415 673.095 512.183 639.605Z" fill="url(#paint1_radial_1376_4815)" />
+                <path d="M621.253 399.063C650.392 491.089 598.779 589.983 505.956 619.941C413.134 649.898 314.27 599.571 285.13 507.544C255.99 415.517 307.604 316.624 400.426 286.666C493.249 256.708 592.113 307.036 621.253 399.063Z" stroke="url(#paint2_linear_1376_4815)" />
+                <path opacity="0.6" d="M797.616 342.142C857.338 530.749 751.554 733.421 561.329 794.815C371.103 856.209 168.486 753.071 108.765 564.464C49.0433 375.857 154.827 173.185 345.052 111.791C535.278 50.3971 737.895 153.535 797.616 342.142Z" stroke="url(#paint3_linear_1376_4815)" />
+                <image href="/images/homeContent2.png" width="600" height="350" clipPath="url(#reliableCircle)" x="18.5%" y="30%" fillOpacity='0.5' />
+                <defs>
+                    <clipPath id="reliableCircle">
+                        <circle cx="454" cy="454" r="150" fill="#FFFFFF" />
+                    </clipPath>
+                    <radialGradient id="paint0_radial_1376_4815" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(393.723 195.087) rotate(72.8188) scale(560.702 566.428)">
+                        <stop stopColor="#2294C3" />
+                        <stop offset="1" stopColor="#2294C3" stopOpacity="0" />
+                    </radialGradient>
+                    <radialGradient id="paint1_radial_1376_4815" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(410.487 281.646) rotate(72.8188) scale(393.929 397.952)">
+                        <stop stopColor="#2294C3" />
+                        <stop offset="1" stopColor="#2294C3" stopOpacity="0" />
+                    </radialGradient>
+                    <linearGradient id="paint2_linear_1376_4815" x1="400.275" y1="286.19" x2="507.953" y2="619.822" gradientUnits="userSpaceOnUse">
+                        <stop stopColor="#159BDE" />
+                        <stop offset="1" stopColor="#159BDE" stopOpacity="0" />
+                    </linearGradient>
+                    <linearGradient id="paint3_linear_1376_4815" x1="344.901" y1="111.314" x2="565.257" y2="794.073" gradientUnits="userSpaceOnUse">
+                        <stop stopColor="#159BDE" />
+                        <stop offset="1" stopColor="#159BDE" stopOpacity="0" />
+                    </linearGradient>
+                </defs>
+            </svg>
+
 
             <div className="relative md:flex items-center mt-20 md:mt-0 px-10 md:px-0 h-full">
                 <div className="flex flex-wrap ">
@@ -43,7 +75,55 @@ const SafeHome: FC = () => {
                     </div>
                 </div>
             </div>
-        </section>
+
+            <div className=" relative md:hidden items-center px-10 md:px-0 h-full mt-[100px] md:mt-0">
+                <div className="flex flex-wrap">
+                    {/* <div className="w-[20%]"></div> */}
+                    <div className="w-full md:w-1/2 m-auto">
+                        <p className="font-lato-light font-light text-[22px] text-white">{t('home.reliable.tip')}</p>
+                        <h1 className="font-lato font-medium text-[32px] text-white uppercase tracking-[0.08em]">{t('home.reliable.title')}</h1>
+                        {
+                            t('home.reliable.description').split('\n').map((item, index) =>
+                                <p className="font-lato font-light tracking-widest text-white text-lg mt-3" key={index}>{item}</p>
+                            )
+                        }
+                    </div>
+                </div>
+            </div>
+
+            <div className="hidden relative md:hidden items-center px-10 md:px-0 h-full">
+                <div className="flex flex-wrap mt-[400px] md:mt-12">
+                    <div className="w-full md:w-1/2 m-auto">
+                        <div className="relative">
+                            <p className="font-lato-light font-light text-[22px] text-white">{t('home.easy.tip')}</p>
+                            <h1 className="font-lato font-medium text-[32px] text-white uppercase tracking-[0.08em]">{t('home.easy.title')}</h1>
+                            {
+                                t('home.easy.description').split('\n').map((item, index) =>
+                                    <p className="font-lato font-light tracking-widest text-white text-lg mt-3" key={index}>{item}</p>
+                                )
+                            }
+                            <div className="hidden md:block md:absolute  top-[62px] left-[110%] pt-9 w-[80%]">
+                                {
+                                    (easyList as unknown as any[]).map((item, index) =>
+                                        <div className="flex mb-12" key={index}>
+                                            <picture>
+                                                <source srcSet="/images/check-icon.svg" type="image/webp" />
+                                                <img src="/images/check-icon.svg" className="w-[28px] h-[22px] mr-[10px]" alt="" />
+                                            </picture>
+                                            <div className="text-white tracking-[0.08em] w-2/3">
+                                                <p className="font-lato text-lg font-medium">{item.split('\n')[0]} </p>
+                                                <p className="font-lato-light text-lg font-light">{item.split('\n')[1]}</p>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
     );
