db421b5046bbb34e6e9051c63be173f6ed5d2d1d
Piglet <piglet20181112@outlook.com>
2022-09-05 05:39:55 -0700

    home animation part 1

--- components/homeComponents/SimplyNetwork.tsx
+++ components/homeComponents/SimplyNetwork.tsx
@@ -1,19 +1,39 @@
 //modules
 import React, { FC } from "react"
 import { useTranslation } from "next-i18next";
-
+import gsap from 'gsap';
 
 // Custom components
 import Breadcrumb from '../../components/common/breadcrumb'
 
 
-const SimplyNetwork: FC = () => {
+const SimplyNetwork: FC<{props?: any, ref: any}> = React.forwardRef((props: any, ref: any) => {
 
     //transalte
     const { t } = useTranslation()
 
+    // animation
+    const getShowTimeline = (duration: number=1.5) => {
+        return gsap.timeline({onReverseComplete: ()=>{gsap.set([containerRef.current], {display: 'none'});}})
+            .fromTo(
+                containerRef.current,
+                {y: -100, opacity: 0},
+                {y: 0, opacity: 1, duration},
+                0
+            )
+    }
+
+    const containerRef = React.useRef<any>();
+    const prevAnimation = React.useRef<any>(null);
+    const startAnim = (direction: string, shown: boolean) => {
+        if ( prevAnimation.current ) prevAnimation.current.kill();
+        gsap.set([containerRef.current], {display: 'block'});
+        if ( direction == 'UP' && shown ) prevAnimation.current = getShowTimeline().play(0);
+        else if (direction == 'DOWN' && !shown ) prevAnimation.current = getShowTimeline().reverse(0);
+    }
+
     return (
-        <section className="container mx-auto pb-16 relative">
+        <section ref={(el)=>{containerRef.current=el; if (ref) ref.current = {container: el, startAnim}}} className="container mx-auto pb-16 relative md:fixed md:hidden md:left-[50%] md:translate-x-[-50%]">
             <svg className="absolute right-[-36%] bottom-[-20%] md:top-1/2 md:translate-y-[-50%] md:w-[100%] md:right-[-53%]  xl:w-[120%]   sm:top-[37%] sm:w-[120%] sm:right-[-12%] w-[174%]  xl:right-[-44%] " viewBox="0 0 817 828" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path opacity="0.6" d="M259.493 676.355C116.557 593.32 67.3899 408.41 149.686 263.345C231.983 118.279 414.572 68.0028 557.507 151.038C700.443 234.072 749.61 418.982 667.314 564.048C585.017 709.114 402.428 759.389 259.493 676.355Z" stroke="url(#paint0_linear_1376_4715)" />
                 <path opacity="0.6" d="M156.234 267.676C76.0933 408.942 124.025 588.918 263.293 669.663C402.561 750.408 580.427 701.346 660.568 560.079C740.709 418.813 692.777 238.837 553.509 158.092C414.241 77.3475 236.375 126.41 156.234 267.676Z" fill="url(#paint1_radial_1376_4715)" />
@@ -69,6 +89,6 @@ const SimplyNetwork: FC = () => {
         </section>
 
     );
-};
+});
 
 export default SimplyNetwork
