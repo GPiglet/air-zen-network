0bb7656b1745d482511fc8f99f4d44cd42b1d702
Piglet <piglet20181112@outlook.com>
2022-09-08 00:59:35 -0700

    mobile cookie

--- components/common/sticky/index.tsx
+++ components/common/sticky/index.tsx
@@ -10,10 +10,10 @@ const Sticky: FC = () => {
     const { changeCookieAllow } = useContext(StoreContext)
 
     return (
-        <div className="hidden md:block bg-black fixed bottom-0 w-full py-7 z-60 border border-third border-opacity-30">
-            <div className="container mx-auto flex  pl-16">
-                <div className="flex flex-col mr-16">
-                    <button className="w-max font-lato text-white text-base bg-gradient-to-r from-secondary to-[#669AB4] py-2.5 px-7 rounded-md" onClick={() => changeCookieAllow?.(true)}>
+        <div className="md:block bg-black fixed bottom-0 w-full py-7 z-60 border border-third border-opacity-30">
+            <div className="container mx-auto flex  md:pl-16 flex-col-reverse md:flex-row">
+                <div className="flex flex-col mx-16 md:ml-0 mt-5 md:mt-0">
+                    <button className="w-full md:w-max font-lato text-white text-base bg-gradient-to-r from-secondary to-[#669AB4] py-2.5 px-7 rounded-md" onClick={() => changeCookieAllow?.(true)}>
                         {t('cookie.accept')}
                     </button>
                     <div className="bg-gradient-to-r w-full p-[1px] rounded-md from-secondary to-[#669AB4] mt-3">
@@ -23,7 +23,7 @@ const Sticky: FC = () => {
 
                     </div>
                 </div>
-                <span className="font-lato font-light text-base text-white tracking-[2px]">{t('cookie.content')}<a href="" className='underline'>{t('cookie.detail')}</a></span>
+                <span className="px-5 md:px-0 font-lato font-light text-xs leading-4 md:text-base text-white tracking-[2px]">{t('cookie.content')}<a href="" className='underline'>{t('cookie.detail')}</a></span>
             </div>
         </div>
     )
