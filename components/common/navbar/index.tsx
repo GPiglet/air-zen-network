50621bf66863801d7ab4f549aaffd9df98ef661a
Piglet <piglet20181112@outlook.com>
2022-09-09 02:50:54 -0700

    backdrop blur

--- components/common/navbar/index.tsx
+++ components/common/navbar/index.tsx
@@ -100,7 +100,7 @@ const Navbar: FC<NaveProps> = ({ navItems }) => {
         </div>
         <div
           className={
-            "fixed md:static w-screen md:w-auto h-screen md:h-auto top-0 md:top-auto left-0 md:left-auto md:flex flex-grow items-center backdrop-brightness-[0.2] md:backdrop-brightness-100 md:bg-opacity-0 md:shadow-none pt-[8px]" +
+            "fixed md:static w-screen md:w-auto h-screen md:h-auto top-0 md:top-auto left-0 md:left-auto md:flex flex-grow items-center backdrop-blur-sm md:backdrop-blur-none md:bg-opacity-0 md:shadow-none pt-[8px]" +
             (navbarOpen ? " block" : " hidden")
           }
         >
