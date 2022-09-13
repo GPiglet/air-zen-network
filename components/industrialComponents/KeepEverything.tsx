66f5d39dc90670ca624fac3f323e7d63170fdf65
Smarttabdev <smarttabdev@gmail.com>
2022-09-13 08:34:34 -0700

    layout

--- components/industrialComponents/KeepEverything.tsx
+++ components/industrialComponents/KeepEverything.tsx
@@ -51,7 +51,7 @@ const KeepEverything: FC<{ props?: any, ref: any }> = React.forwardRef((props: a
 
     return (
         <div ref={(el) => { containerRef.current = el; if (ref) ref.current = { container: el, startAnim } }} className="pb-16 relative container mx-auto md:h-screen md:fixed md:hidden md:left-[50%] md:translate-x-[-50%]">
-            <svg className="absolute hidden md:block top-[30%] md:w-[20%]"
+            <svg className="absolute hidden md:block top-[30%] left-[-13%] md:w-[23%]"
                 viewBox="0 0 356 374" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path opacity="0.8" d="M212.963 306.349C274.488 286.66 308.704 221.66 289.387 161.169C270.07 100.678 204.535 67.601 143.01 87.2905C81.4851 106.98 47.2689 171.979 66.5859 232.471C85.9029 292.962 151.438 326.039 212.963 306.349Z" fill="url(#paint0_radial_1376_7680)" />
                 <path opacity="0.8" d="M251.813 194.561C298.808 179.522 324.945 129.872 310.189 83.6651C295.434 37.4585 245.375 12.1929 198.379 27.2328C151.383 42.2726 125.247 91.9226 140.002 138.129C154.757 184.336 204.817 209.601 251.813 194.561Z" fill="url(#paint1_radial_1376_7680)" />
@@ -75,8 +75,8 @@ const KeepEverything: FC<{ props?: any, ref: any }> = React.forwardRef((props: a
             <div className="container mx-auto relative items-center text-white px-10 md:px-0 pt-[150px] md:pt-0 md:absolute md:top-1/2 md:translate-y-[-50%]">
                 <Breadcrumb />
                 <div className="md:flex items-center mt-5 ">
-                    <div className="md:w-[350px] xl:w-[40%]">
-                        <h1 className="text-title-md">
+                    <div className="md:w-[350px] xl:w-[30%]">
+                        <h1 className="text-title-md w-[90%]">
                             {t('industrial.everything.title')}
                         </h1>
                         <p className="font-lato font-light tracking-widest text-lg mt-3">
@@ -102,9 +102,9 @@ const KeepEverything: FC<{ props?: any, ref: any }> = React.forwardRef((props: a
                 </div>
             </div>
             <svg className="md:absolute 
-            right-[-35%] md:right-[-35%]  xl:right-[-27%] z-10
+            right-[-35%] md:right-[-35%]  xl:right-[-47%] z-10
             top-[90%] xs:top-[75%] sm:top-[65%] md:top-1/2 md:translate-y-[-50%]
-            w-[110%] md:w-[90%] xl:w-[85%]"
+            w-[110%] md:w-[90%] xl:w-[105%]"
                 viewBox="0 0 1000 600" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path opacity="0.8" d="M678.873 519.526C809.282 477.167 881.807 337.33 840.862 207.192C799.918 77.0538 661.008 5.89465 530.599 48.2536C400.19 90.6125 327.665 230.449 368.609 360.587C409.554 490.725 548.464 561.885 678.873 519.526Z" fill="url(#paint0_radial_0_1)" />
                 <path opacity="0.8" d="M792.368 222.938C824.906 326.358 767.272 437.49 663.631 471.154C559.989 504.818 449.597 448.265 417.059 344.845C384.52 241.425 442.155 130.293 545.796 96.629C649.437 62.9648 759.829 119.518 792.368 222.938Z" stroke="url(#paint1_linear_0_1)" />
