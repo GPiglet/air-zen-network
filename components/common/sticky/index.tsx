adc7677672b6666ebd796194af082122cfb223b8
zenithorr <87866630+zenithorr@users.noreply.github.com>
2022-09-18 18:18:54 -0700

    cookie (#64)
    * multi language
    
    * home subpage form ongoing
    
    * home subpage form section demo
    
    * multilang
    
    * overflowclip
    
    * deploy error fixed
    
    * deploy error fixed
    
    * deploy error fixed
    
    * deploy error fixed
    
    * industrial subpage backgroundcolor
    
    * fix aboutus touch issue
    
    * multi language demo
    
    * multi language demo
    
    * multi language demo
    
    * multi language demo
    
    * fix some issue for deploy
    
    * fix some issue for deploy
    
    * fix some issue for deploy
    
    * multilanguage key modified
    
    * subpage Navitem & equal box sizing & footer missing & back to solution anchor
    
    * navbar blur
    
    * navbar blur
    
    * font-lato-italic& solution box opening & ..
    
    * mobile top menu hide show &&
    
    * checkbox custom
    
    * checkbox custom
    
    * top navbar global
    
    * story tail fix
    
    * landing page layout fixed
    
    * landing page layout fixed
    
    * about us layout fixed
    
    * solutions desktop ani css remove
    
    * layout fixed
    
    * checkbox exam
    
    * checkbox exam
    
    * checkbox exam
    
    * checkbox testing
    
    * checkbox testing
    
    * checkbox testing
    
    * checkbox testing
    
    * checkbox testing
    
    * checkbox changed
    
    * fix input border gradient opacity
    
    * breadcrumb issue
    
    * fix some issues
    
    * fix some issues
    
    * font issue
    
    * font issue
    
    * font issue
    
    * font issue
    
    * font issue
    
    * font issue
    
    * font issue
    
    * reference issue
    
    * reference issue
    
    * layout
    
    * layout
    
    * layout
    
    * layout
    
    * business form
    
    * cookie lang fix
    
    * scroll issue
    
    * fix
    
    * 2 subpages desktop layout
    
    * layout
    
    * layot
    
    * layout
    
    * layout
    
    * landing page mobile issue
    
    * landing mobile
    
    * mobile layout fix
    
    * landing
    
    * change cookie
    
    Co-authored-by: Smarttabdev <87866630+Smarttabdev@users.noreply.github.com>

--- components/common/sticky/index.tsx
+++ components/common/sticky/index.tsx
@@ -1,23 +1,25 @@
-import React, { FC, useContext, useTransition } from 'react'
-
+import React, { FC, useContext, useState, useTransition } from 'react'
 import { StoreContext } from '../../../contexts/Store'
 import { useTranslation } from 'next-i18next'
+
+import { setStorage } from '../../../services/storage.service'
 
 
 const Sticky: FC = () => {
 
     const { t } = useTranslation()
-    const { changeCookieAllow } = useContext(StoreContext)
+    const [show, setShow] = useState(true)
+    //const { changeCookieAllow } = useContext(StoreContext)
 
     return (
-        <div className="md:block bg-black fixed bottom-0 w-full py-7 z-60 border border-third border-opacity-30">
+        <div className={`bg-black fixed bottom-0 w-full py-7 z-60 border border-third border-opacity-30 ${show ? 'block' : 'hidden'}`}>
             <div className="container mx-auto flex  md:pl-16 flex-col-reverse md:flex-row">
                 <div className="flex flex-col mx-16 md:ml-0 mt-5 md:mt-0">
-                    <button className="w-full md:w-max font-lato text-white text-base bg-gradient-to-r from-secondary to-[#669AB4] py-2.5 px-7 rounded-md" onClick={() => changeCookieAllow?.(true)}>
+                    <button className="w-full md:w-max font-lato text-white text-base bg-gradient-to-r from-secondary to-[#669AB4] py-2.5 px-7 rounded-md" onClick={() => { setStorage('cookieAllow', true); setShow(false) }}>
                         {t('cookie.accept')}
                     </button>
                     <div className="bg-gradient-to-r w-full p-[1px] rounded-md from-secondary to-[#669AB4] mt-3">
-                        <button className=" font-lato w-full text-white text-base bg-black py-2.5 border- rounded-md" onClick={() => changeCookieAllow?.(true)}>
+                        <button className=" font-lato w-full text-white text-base bg-black py-2.5 border- rounded-md" onClick={() => { setStorage('cookieAllow', true), setShow(false) }}>
                             {t('cookie.customize')}
                         </button>
 
