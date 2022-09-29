6b030bafb1fbe3617a6d7ae851d693bac6a6bad0
Piglet <piglet20181112@outlook.com>
2022-09-29 15:30:41 -0700

    #101, make 14 fontsize

--- components/common/sparkle/index.tsx
+++ components/common/sparkle/index.tsx
@@ -9,7 +9,7 @@ const Sparkle :FC<SparkleProps> = ({className, children}) => {
     return (
       <div className={`hidden md:flex items-start ${className ? className : ''}`}>
         <img src="/images/sparkle.svg" className="relative top-[-9px] w-[40px]" />
-        <p className='font-lato-light w-full text-base text-white'>{children}</p>
+        <p className='font-lato-light w-full text-sm text-white'>{children}</p>
       </div>
     );
   };
